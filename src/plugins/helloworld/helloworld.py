5f134b92f6539cd9377bdf1af1a48733d8737c91
Matt Walters <me@mattwalters.net>
2020-10-12 19:04:29 -0400

    Update documentation in example plugin

--- src/plugins/helloworld/helloworld.py
+++ src/plugins/helloworld/helloworld.py
@@ -7,7 +7,9 @@ import time, datetime
 # You MUST inherit PytaskrPlugin
 # You MUST choose a unique plugin name and it must match the filename
 class HelloWorld(PytaskrPlugin):
+    # You MUT declare this `meta` attribute
     meta = {}
+
     # You MUT implement a `perform` method
     async def perform(self):
         # Do whatever you want your plugin to do
