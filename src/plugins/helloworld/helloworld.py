74db119cb1ce795000eed282a76a0ba2562656ad
Matt Walters <me@mattwalters.net>
2020-10-13 12:47:13 -0400

    Change `interval` naming to `crontab` so its purpose is more obvious

--- src/plugins/helloworld/helloworld.py
+++ src/plugins/helloworld/helloworld.py
@@ -2,7 +2,7 @@
 from support.pytaskrplugin import PytaskrPlugin
 
 # You SHOULD import modules your plugin requires
-import time, datetime
+import sys, time, datetime
 
 # You MUST inherit PytaskrPlugin
 # You MUST choose a unique plugin name and it must match the filename
@@ -14,7 +14,8 @@ class HelloWorld(PytaskrPlugin):
     async def perform(self):
         # Do whatever you want your plugin to do
         print("Someone is coming ...")
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        sys.stdout.flush()
         time.sleep(15)
         print("Hello, world!")
+        sys.stdout.flush()
         return True
